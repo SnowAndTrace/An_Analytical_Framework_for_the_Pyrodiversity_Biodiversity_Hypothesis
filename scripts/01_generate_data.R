3f9de95289e218a02068ae09b66978afd659bdab
SnowAndTrace <82654911+SnowAndTrace@users.noreply.github.com>
2026-03-01 16:24:00 +1100

    add the case of b = 0.01 and b = 0.005 (representing long time since the most recent fire))

--- scripts/01_generate_data.R
+++ scripts/01_generate_data.R
@@ -8,7 +8,7 @@ alpha_list <- data$alpha #
 
 # 2. Define Parameters for the grid
 k_vals <- seq(0.3, 3, length.out = 10000) #
-b_fixed_vals <- c(0.5, 0.9, 1, 3) 
+b_fixed_vals <- c(0.005, 0.01, 0.5, 0.9, 1, 3) 
 
 # Create an empty dataframe to store results
 all_trajectories <- data.frame()
